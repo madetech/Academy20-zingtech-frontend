5038f55fca0f579fdeb2929bc39fa23394e777a9
Emma Blott <blott23@gmail.com>
2020-10-28 15:57:32 +0000

    Add simple GOV button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'govuk-react';
 
 function App() {
+  const MyComponent = ({title}) => (<div>
+    <h1>{title}</h1>
+    <Button />
+  </div>)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MyComponent title="Emmas Button" > Blah blah Blah </ MyComponent >
     </div>
   );
 }
