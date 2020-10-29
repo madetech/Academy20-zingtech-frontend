08ba3776501e06bd6630a05dd9d60dd15738f43f
James Francis <james.franics@madetech.com>
2020-10-29 10:29:02 +0000

    adding test for emma

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Claire is here', () => {
   render(<App />);
   const linkElement = screen.getByText(/Claire/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Emma is here', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/emma/i);
   expect(linkElement).toBeInTheDocument();
 });
