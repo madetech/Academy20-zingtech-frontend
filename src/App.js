398604e11af1f45ec51b59c93244b441f0160344
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-11-03 11:42:27 +0000

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,55 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, } from "react-router-dom";
+
+// Gov.UK React components
 import Page from '@govuk-react/page';
 import BackLink from '@govuk-react/back-link';
 import { H1 } from '@govuk-react/heading';
 import Footer from '@govuk-react/footer';
-
 import Table from '@govuk-react/table';
-
-
-
 
 function App() {
   return (
-    <div>
-      <Page beforeChildren={<BackLink href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" />}>
-        <H1>HMRC HR - BOOM🤯</H1>
-        <Table caption="Employee data">
-          <Table.Row>
-            <Table.CellHeader>Full Name</Table.CellHeader>
-            <Table.CellHeader>Employee ID</Table.CellHeader>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>Emma Blott</Table.Cell>
-            <Table.Cell>41908374</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>James Francis</Table.Cell>
-            <Table.Cell>54245753</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>Zack Adlington</Table.Cell>
-            <Table.Cell>457245457</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>Claire Guest</Table.Cell>
-            <Table.Cell>54245753</Table.Cell>
-          </Table.Row>
-        </Table>
+    <Router>
+      <Fragment>
+        <Page beforeChildren={<BackLink href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" />}>
+          <Route exact path="/">
+            <p>Login component goes here</p>
+          </Route>
+          <Route exact path ="/employees">
+            <H1>HMRC HR - BOOM🤯</H1>
+            <Table caption="Employee data">
+              <Table.Row>
+                <Table.CellHeader>Full Name</Table.CellHeader>
+                <Table.CellHeader>Employee ID</Table.CellHeader>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>Emma Blott</Table.Cell>
+                <Table.Cell>41908374</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>James Francis</Table.Cell>
+                <Table.Cell>54245753</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>Zack Adlington</Table.Cell>
+                <Table.Cell>457245457</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>Claire Guest</Table.Cell>
+                <Table.Cell>54245753</Table.Cell>
+              </Table.Row>
+            </Table>
+          </Route>
 
+        </Page> 
 
-      </Page> 
-
-      <Footer meta={
-    <Footer.MetaCustom>
-      Built by the Bristol ZingTech as a learning experience
-    </Footer.MetaCustom>
-  }
-/>
-    </div>
-
-
+        <Footer meta={
+            <Footer.MetaCustom>
+              Built by the Bristol ZingTech as a learning experience
+            </Footer.MetaCustom>
+          }
+        />
+      </Fragment>
+    </Router>
   );
 }
 
