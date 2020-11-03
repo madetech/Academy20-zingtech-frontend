a52001d9587f9cd1863f71b1801e81c5f5c3d7de
Emma Blott <blott23@gmail.com>
2020-11-03 15:12:50 +0000

    Profile component from data json complete

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,5 @@
 import Table from '@govuk-react/table';
+import { Link } from "react-router-dom";
 
 const data = {
   "employee_type": "Employee",
@@ -11,7 +12,7 @@ const data = {
   "next_of_kin": "San Pedro River Sandmat",
   "next_of_kin_contact_number": "07546 555 653",
   "salary": "£89676.03",
-  "start_date": "22/06/1997",
+  "date_of_birth": "22/06/1997",
   "office location": "Chendong"
 }
 
@@ -21,6 +22,27 @@ const Profile = (props) => (
     <Table.Row>
       <Table.CellHeader>Name</Table.CellHeader>
       <Table.Cell>{`${data.first_name} ${data.last_name}`}</Table.Cell>
+      <Table.Cell><Link href="#">Change</Link></Table.Cell>
+    </Table.Row>
+    <Table.Row>
+      <Table.CellHeader>Date of Birth</Table.CellHeader>
+      <Table.Cell>{data.date_of_birth}</Table.Cell>
+      <Table.Cell></Table.Cell>
+    </Table.Row>
+    <Table.Row>
+      <Table.CellHeader>Home Address</Table.CellHeader>
+      <Table.Cell>{data.address}</Table.Cell>
+      <Table.Cell><Link href="#">Change</Link></Table.Cell>
+    </Table.Row>
+    <Table.Row>
+      <Table.CellHeader>Contact Details</Table.CellHeader>
+      <Table.Cell>{data.contact_number}<br/><br/>{data.email}</Table.Cell>
+      <Table.Cell><Link href="#">Change</Link></Table.Cell>
+    </Table.Row>
+    <Table.Row>
+      <Table.CellHeader>Next of kin</Table.CellHeader>
+      <Table.Cell>{data.next_of_kin}<br/><br/>{data.next_of_kin_contact_number}</Table.Cell>
+      <Table.Cell><Link href="#">Change</Link></Table.Cell>
     </Table.Row>
     </Table>
   </>
