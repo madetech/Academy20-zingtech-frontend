ff0a4fe929076e4fda67e0628a8b36de05ac7a9e
Emma Blott <blott23@gmail.com>
2020-11-04 11:24:31 +0000

    delete comments

--- src/pages/MyEmployeesPage.js
+++ src/pages/MyEmployeesPage.js
@@ -9,7 +9,6 @@ import APIGateway from "../APIGateway.js"
 
 const NavLink = asNavLinkAnchor(Link);
 
-// const columns = ["Name", "Company", "City", "State"];
 const columns = [
   {
    name: "id",
@@ -45,12 +44,7 @@ const columns = [
   },
  ];
 const data = APIGateway.get_employees();
-// const data = [
-//   ["Joe James", "Test Corp", "Yonkers", "NY"],
-//   ["John Walsh", "Test Corp", "Hartford", "CT"],
-//   ["Bob Herm", "Test Corp", "Tampa", "FL"],
-//   ["James Houston", "Test Corp", "Dallas", "TX"],
-//  ];
+
 const options = {
     filterType: 'checkbox',
 };
