4e38695d7fd0a7866085236d221ede54b2cd1ec5
James Francis <james.franics@madetech.com>
2020-11-19 15:09:39 +0000

    adding remaining fields to create employee page and temp post request

--- src/components/CreateEmployeeForm.js
+++ src/components/CreateEmployeeForm.js
@@ -7,10 +7,22 @@ import Button from "@govuk-react/button";
 
 const emptyEmployeeObject = {
   firstName: null,
-  lastName: null
-}
+  lastName: null,
+  email: null,
+  mobileNumber: null,
+  address: null,
+  city: null,
+  postcode: null,
+  nextOfKinName: null,
+  nextOfKinContactNumber: null,
+  salaryBand: null,
+  officeLocation: null,
+  position: null,
+  userType: null,
+  manager: null,
+};
 
-function CreateEmployeeForm() {
+function CreateEmployeeForm({ sendToBackend }) {
   const [employeeObject, setEmployeeObject] = useState(emptyEmployeeObject);
 
   return (
@@ -22,7 +34,9 @@ function CreateEmployeeForm() {
         <InputField
           name="firstName"
           value={employeeObject.firstName}
-          onChange={(e) => setEmployeeObject({...employeeObject, firstName:e.target.value})}
+          onChange={(e) =>
+            setEmployeeObject({ ...employeeObject, firstName: e.target.value })
+          }
         >
           First name
         </InputField>
@@ -30,19 +44,184 @@ function CreateEmployeeForm() {
         <InputField
           name="lastName"
           value={employeeObject.lastName}
-          onChange={(e) => setEmployeeObject({...employeeObject, lastName:e.target.value})}
+          onChange={(e) =>
+            setEmployeeObject({ ...employeeObject, lastName: e.target.value })
+          }
         >
           Last name
         </InputField>
         <br />
+
+        <InputField
+          name="email"
+          value={employeeObject.email}
+          onChange={(e) =>
+            setEmployeeObject({ ...employeeObject, email: e.target.value })
+          }
+        >
+          Email
+        </InputField>
+        <br />
+
+        <InputField
+          name="mobileNumber"
+          value={employeeObject.mobileNumber}
+          onChange={(e) =>
+            setEmployeeObject({
+              ...employeeObject,
+              mobileNumber: e.target.value,
+            })
+          }
+        >
+          Mobile Number
+        </InputField>
+        <br />
+
+        <InputField
+          name="address"
+          value={employeeObject.address}
+          onChange={(e) =>
+            setEmployeeObject({ ...employeeObject, address: e.target.value })
+          }
+        >
+          Address
+        </InputField>
+        <br />
+
+        <InputField
+          name="city"
+          value={employeeObject.city}
+          onChange={(e) =>
+            setEmployeeObject({ ...employeeObject, city: e.target.value })
+          }
+        >
+          City
+        </InputField>
+        <br />
+
+        <InputField
+          name="postcode"
+          value={employeeObject.postcode}
+          onChange={(e) =>
+            setEmployeeObject({ ...employeeObject, postcode: e.target.value })
+          }
+        >
+          Postcode
+        </InputField>
+        <br />
       </Fieldset>
-      <Button
-        onClick={() =>
-          window.alert(
-            `Clicked "Edit" for row ${employeeObject.firstName} with dataIndex of ${employeeObject.lastName}`
-          )
-        }
-      >
+
+      <Fieldset id="nextOfKinInfo">
+        <Fieldset.Legend>
+          <b>Next of Kin Informtion</b>
+        </Fieldset.Legend>
+
+        <InputField
+          name="nextOfKinName"
+          value={employeeObject.nextOfKinName}
+          onChange={(e) =>
+            setEmployeeObject({
+              ...employeeObject,
+              nextOfKinName: e.target.value,
+            })
+          }
+        >
+          Next of Kin Full Name
+        </InputField>
+        <br />
+
+        <InputField
+          name="nextOfKinContactNumber"
+          value={employeeObject.nextOfKinContactNumber}
+          onChange={(e) =>
+            setEmployeeObject({
+              ...employeeObject,
+              nextOfKinContactNumber: e.target.value,
+            })
+          }
+        >
+          Next of Kin Contact Number
+        </InputField>
+        <br />
+      </Fieldset>
+
+      <Fieldset id="employmentDetails">
+        <Fieldset.Legend>
+          <b>Employment Details</b>
+        </Fieldset.Legend>
+
+        <InputField
+          name="salaryBand"
+          value={employeeObject.salaryBand}
+          onChange={(e) =>
+            setEmployeeObject({
+              ...employeeObject,
+              salaryBand: e.target.value,
+            })
+          }
+        >
+          Salary Band
+        </InputField>
+        <br />
+
+        <InputField
+          name="officeLocation"
+          value={employeeObject.officeLocation}
+          onChange={(e) =>
+            setEmployeeObject({
+              ...employeeObject,
+              officeLocation: e.target.value,
+            })
+          }
+        >
+          Office Location
+        </InputField>
+        <br />
+
+        <InputField
+          name="position"
+          value={employeeObject.position}
+          onChange={(e) =>
+            setEmployeeObject({
+              ...employeeObject,
+              position: e.target.value,
+            })
+          }
+        >
+          Position
+        </InputField>
+        <br />
+
+        <InputField
+          name="userType"
+          value={employeeObject.userType}
+          onChange={(e) =>
+            setEmployeeObject({
+              ...employeeObject,
+              userType: e.target.value,
+            })
+          }
+        >
+          User Type
+        </InputField>
+        <br />
+
+        <InputField
+          name="manager"
+          value={employeeObject.manager}
+          onChange={(e) =>
+            setEmployeeObject({
+              ...employeeObject,
+              manager: e.target.value,
+            })
+          }
+        >
+          Manager
+        </InputField>
+        <br />
+      </Fieldset>
+      <br />
+      <Button onClick={() => sendToBackend(employeeObject)}>
         Create Employee
       </Button>
     </div>
--- src/pages/CreateNewEmployeePage.js
+++ src/pages/CreateNewEmployeePage.js
@@ -5,25 +5,42 @@ import Page from "@govuk-react/page";
 
 import CreateEmployeeForm from "../components/CreateEmployeeForm";
 import LoggedInTopNav from "../components/LoggedInTopNav";
+import Axios from "axios";
 
-const CreateNewEmployeePage = (props) => (
-  <>
-    <Page
-      header={<LoggedInTopNav highlighted="employeesTopNav" />}
-      beforeChildren={
-        <Breadcrumbs>
-          <Breadcrumbs.Link href="/">HMRC-HR</Breadcrumbs.Link>
-          <Breadcrumbs.Link id="employeesBreadcrumb" href="/employees">
-            My Employees
-          </Breadcrumbs.Link>
-          Create New Employee
-        </Breadcrumbs>
+function CreateNewEmployeePage() {
+  function sendToBackend(newEmployeeData) {
+    Axios.post(
+      "https://zingtech-backend.herokuapp.com/api/employeedata",
+      newEmployeeData
+    ).then(
+      (res) => {
+        console.log(res);
+      },
+      (error) => {
+        console.log(error);
       }
-    >
-      <Heading>Create new employee</Heading>
-      <CreateEmployeeForm />
-    </Page>
-  </>
-);
+    );
+  }
+
+  return (
+    <>
+      <Page
+        header={<LoggedInTopNav highlighted="employeesTopNav" />}
+        beforeChildren={
+          <Breadcrumbs>
+            <Breadcrumbs.Link href="/">HMRC-HR</Breadcrumbs.Link>
+            <Breadcrumbs.Link id="employeesBreadcrumb" href="/employees">
+              My Employees
+            </Breadcrumbs.Link>
+            Create New Employee
+          </Breadcrumbs>
+        }
+      >
+        <Heading>Create new employee</Heading>
+        <CreateEmployeeForm sendToBackend={sendToBackend} />
+      </Page>
+    </>
+  );
+}
 
 export default CreateNewEmployeePage;
