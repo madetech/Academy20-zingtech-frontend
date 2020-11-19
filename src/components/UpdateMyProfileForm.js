e3c266864181851c7f3cf70cf93d4d07965aab38
Zack Adlington <54271433+zackads@users.noreply.github.com>
2020-11-19 19:34:56 +0000

    Add navigation buttons

--- src/components/UpdateMyProfileForm.js
+++ src/components/UpdateMyProfileForm.js
@@ -1,12 +1,16 @@
+import React from 'react';
+import axios from "axios";
+
 import Fieldset from '@govuk-react/fieldset';
 import DateField from '@govuk-react/date-field';
 import Button from '@govuk-react/button';
-import React from 'react';
-import Link from '@govuk-react/link';
 import Input from '@govuk-react/input';
 import Label from '@govuk-react/label';
 import LabelText from '@govuk-react/label-text';
-import axios from "axios";
+import Link from '@govuk-react/link';
+import Panel from '@govuk-react/panel';
+
+import ReviewForm from './ReviewForm';
 
 class UpdateMyProfileForm extends React.Component { 
   constructor(props) {
@@ -14,13 +18,17 @@ class UpdateMyProfileForm extends React.Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleReset = this.handleReset.bind(this);
+    this.handleReview = this.handleReview.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
 
     this.state = {
       // TODO: Update current_user when authentication implemented
       current_user: 10000,
       loaded: false,
       initial: {},
-      updated: {}
+      updated: {},
+      editing: true,
+      submitted: false
     }
   }
 
@@ -29,7 +37,8 @@ class UpdateMyProfileForm extends React.Component {
     .then(({ data }) => {
       this.setState({
         loaded: true,
-        initial: data
+        initial: data,
+        updated: data
       });
     });
   }
@@ -42,64 +51,102 @@ class UpdateMyProfileForm extends React.Component {
     })
   }
 
-  handleReset(e) {
+  handleReset() {
     this.setState({
+      editing: true,
       updated: this.state.initial
     });
   }
 
+  handleReview() {
+    this.setState({
+      editing: false,
+      updated: {...this.state.initial, ...this.state.updated}
+    });
+    console.log(this.state);
+  }
+
+  handleSubmit() {
+    // TODO: POST request to API
+    this.setState({
+      submitted: true
+    })
+  }
+
   render() {
-    let { initial, updated, loaded } = this.state;
+    const { initial, updated } = this.state;
 
-    return !loaded ? <p>Loading...</p> : <div id="">
-        <Fieldset>
-          <h2>About you</h2>
-          <Label >
-            <LabelText>First name</LabelText>
-            <Input name="firstName" defaultValue={ initial.firstName } value={ updated.firstName } onChange={ this.handleChange }/>
-          </Label><br/>
-          <Label >
-            <LabelText>Last name</LabelText>
-            <Input name="lastName" defaultValue={ initial.lastName } value={ updated.lastName } onChange={ this.handleChange } />
-          </Label><br/>
+    return <div id="">
+            { this.state.loaded && this.state.editing && <>
+              <Fieldset>
+                <h2>About you</h2>
+                <Label >
+                  <LabelText>First name</LabelText>
+                  <Input name="firstName" defaultValue={ initial.firstName } value={ updated.firstName } onChange={ this.handleChange }/>
+                </Label><br/>
+                <Label >
+                  <LabelText>Last name</LabelText>
+                  <Input name="lastName" defaultValue={ initial.lastName } value={ updated.lastName } onChange={ this.handleChange } />
+                </Label><br/>
 
-          <DateField defaultValues={{
-            // TODO: API needs to return date of birth
-            day: "31",
-            month: "10",
-            year: "1982"
-          }}>
-            Date of birth
-          </DateField>
+                <DateField defaultValues={{
+                  // TODO: API needs to return date of birth
+                  day: "31",
+                  month: "10",
+                  year: "1982"
+                }}>
+                  Date of birth
+                </DateField>
 
-          <h2>Your contact details</h2>
-          <Label>
-            <LabelText>Address 1</LabelText>
-            <Input name="address" defaultValue={ initial.address } value={ updated.address } onChange={ this.handleChange } />
-          </Label><br/>
-          <Label>
-            <LabelText>Address 2</LabelText>
-            <Input value="" />
-          </Label><br/>
-          <Label>
-            <LabelText>City</LabelText>
-            <Input name="city" defaultValue={ initial.city } value={ updated.city } onChange={ this.handleChange } />
-          </Label><br/>
+                <h2>Your contact details</h2>
+                <Label>
+                  <LabelText>Address 1</LabelText>
+                  <Input name="address" defaultValue={ initial.address } value={ updated.address } onChange={ this.handleChange } />
+                </Label><br/>
+                <Label>
+                  <LabelText>Address 2</LabelText>
+                  <Input value="" />
+                </Label><br/>
+                <Label>
+                  <LabelText>City</LabelText>
+                  <Input name="city" defaultValue={ initial.city } value={ updated.city } onChange={ this.handleChange } />
+                </Label><br/>
+                <Label>
+                  <LabelText>Postcode</LabelText>
+                  <Input name="postcode" defaultValue={ initial.postcode } value={ updated.postcode } onChange={ this.handleChange } />
+                </Label><br/>
 
-          <h2>Your next of kin</h2>
-          <Label>
-            <LabelText>Name</LabelText>
-            <Input name="nextOfKin" defaultValue={ initial.nextOfKin } value={ updated.nextOfKin } onChange={ this.handleChange } />
-          </Label><br/>
-          <Label>
-            <LabelText>Contact telephone number</LabelText>
-            <Input name="nextOfKinContactNumber" defaultValue={ initial.nextOfKinContactNumber } value={ updated.nextOfKinContactNumber } onChange={ this.handleChange } />
-          </Label><br/>
-        </Fieldset>
-        <br/ >
-        <Button onClick={ this.handleReset }>Reset</Button>
-        <Button onClick={ this.handleSubmit }>Review and confirm</Button>
-    </div>
+                <h2>Your next of kin</h2>
+                <Label>
+                  <LabelText>Name</LabelText>
+                  <Input name="nextOfKin" defaultValue={ initial.nextOfKin } value={ updated.nextOfKin } onChange={ this.handleChange } />
+                </Label><br/>
+                <Label>
+                  <LabelText>Contact telephone number</LabelText>
+                  <Input name="nextOfKinContactNumber" defaultValue={ initial.nextOfKinContactNumber } value={ updated.nextOfKinContactNumber } onChange={ this.handleChange } />
+                </Label><br/>
+              </Fieldset>
+              <br/ >
+              <Link href="/home"><Button>Cancel</Button></Link>
+              <Button onClick={ this.handleReset }>Reset</Button>
+              <Button onClick={ this.handleReview }>Review and confirm</Button>
+              </>
+            }
+            { !this.state.editing && !this.state.submitted && <>
+              <ReviewForm data={ updated } />
+              <Link href="/home"><Button>Cancel</Button></Link>
+              <Button onClick={ this.handleReset }>Back</Button>
+              <Button onClick={ this.handleSubmit }>Confirm</Button>
+              </>
+            }
+            { this.state.submitted && <>
+              <Panel title="Your details have been updated">
+                Your reference number is<br />
+                <strong>74893</strong>
+              </Panel>
+              </>
+            }  
+          </div>
   }
 }
 
--- src/pages/UpdateMyProfilePage.js
+++ src/pages/UpdateMyProfilePage.js
@@ -2,6 +2,7 @@ import Heading from '@govuk-react/heading';
 import Breadcrumbs from '@govuk-react/breadcrumbs';
 import Link from '@govuk-react/link';
 import Page from '@govuk-react/page';
+import PhaseBanner from '@govuk-react/phase-banner';
 
 import UpdateMyProfileForm from '../components/UpdateMyProfileForm';
 import LoggedInTopNav from "../components/LoggedInTopNav";
@@ -12,11 +13,14 @@ const UpdateMyProfilePage = () => (
     header={<LoggedInTopNav highlighted="myProfileTopNav" />}
     beforeChildren=
         {
+        <>
         <Breadcrumbs>
             <Breadcrumbs.Link href="/">HMRC-HR</Breadcrumbs.Link>
             <Breadcrumbs.Link href="/home">My profile</Breadcrumbs.Link>
             Update my profile
         </Breadcrumbs>
+        <PhaseBanner level="alpha">This feature is unfinished: it gets existing data from the database via the API but does not submit to it.</PhaseBanner>
+        </>
         }
     >
     <Heading>Update my profile</Heading> 
